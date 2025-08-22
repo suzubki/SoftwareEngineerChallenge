fce4fbca8ac5120eb9ab76b48e44c3bf7a3455e5
Darwin_N19 <darwinccn25@gmail.com>
2025-08-22 12:29:58 -0500

    add setup schema and model

--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -22,8 +22,19 @@ export const CreatePostSchema = createInsertSchema(Post, {
   updatedAt: true,
 });
 
+export const Setup = pgTable("setup", (t) => ({
+  id: t.uuid().notNull().primaryKey().defaultRandom(),
+  title: t.varchar({ length: 256 }).notNull(),
+  author: t.varchar({ length: 256 }).notNull(),
+  imageUrl: t.text().notNull(),
+  description: t.text().notNull(),
+  likes: t.integer().notNull().default(0),
+  tags: t.text().array().notNull(),
+}));
+
 export interface EntityMap {
   Post: typeof Post.$inferSelect;
+  Setup: typeof Setup.$inferSelect;
 }
 export type Entity = keyof EntityMap;
 export type Model<TEntity extends Entity> = EntityMap[TEntity];
