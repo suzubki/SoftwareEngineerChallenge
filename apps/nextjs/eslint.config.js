d188a33b7789f5c96c2809025590dab942037f17
Darwin_N19 <darwinccn25@gmail.com>
2025-08-22 20:44:52 -0500

    linter: add rule to ignore

--- apps/nextjs/eslint.config.js
+++ apps/nextjs/eslint.config.js
@@ -9,6 +9,7 @@ export default [
     rules: {
       "@typescript-eslint/no-unsafe-call": "off",
       "@typescript-eslint/no-unsafe-argument": "off",
+      "@typescript-eslint/no-unnecessary-condition": "off",
     },
   },
   ...baseConfig,
